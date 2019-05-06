61160eef513226d35fd760e5f8d93fcf27582e01
Bohan Yang <youthdna@live.com>
2019-05-07 01:43:15 +0800

    Update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,12 +7,10 @@ if [ -z "$V2RAY_UUID" ]; then
   echo "UUID: $V2RAY_UUID"
 fi
 
-if [ -z "$V2RAY_PATH" ]; then
-  V2RAY_PATH=$(uuidgen | tr -d '-')
-  echo "PATH: /$V2RAY_PATH"
-fi
-
 sed -i "s,23ad6b10-8d1a-40f7-8ad0-e3e35cd38297,$V2RAY_UUID," /etc/v2ray/config.json
-sed -i "s,/v2ray,$V2RAY_PATH," /etc/v2ray/config.json
+
+if [ -n "$V2RAY_PATH" ]; then
+  sed -i "s,/v2ray,$V2RAY_PATH," /etc/v2ray/config.json
+fi
 
 exec "$@"
