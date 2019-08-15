2a42c6c1f618c6205a4276c34745c912cccb1d76
Bohan Yang <youthdna@live.com>
2019-08-15 15:38:53 +0800

    Add $V2RAY_CONFIG_URL

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,15 +2,19 @@
 
 set -e
 
-if [ -z "$V2RAY_UUID" ]; then
-  V2RAY_UUID="$(cat /proc/sys/kernel/random/uuid)"
-  echo "UUID: $V2RAY_UUID"
-fi
+if [ -n "$V2RAY_CONFIG_URL" ]; then
+  wget -O /etc/v2ray/config.json "$V2RAY_CONFIG_URL"
+else
+  if [ -z "$V2RAY_UUID" ]; then
+    V2RAY_UUID="$(cat /proc/sys/kernel/random/uuid)"
+    echo "UUID: $V2RAY_UUID"
+  fi
 
-sed -i "s,23ad6b10-8d1a-40f7-8ad0-e3e35cd38297,$V2RAY_UUID," /etc/v2ray/config.json
+  sed -i "s,23ad6b10-8d1a-40f7-8ad0-e3e35cd38297,$V2RAY_UUID," /etc/v2ray/config.json
 
-if [ -n "$V2RAY_PATH" ]; then
-  sed -i "s,/v2ray,$V2RAY_PATH," /etc/v2ray/config.json
+  if [ -n "$V2RAY_PATH" ]; then
+    sed -i "s,/v2ray,$V2RAY_PATH," /etc/v2ray/config.json
+  fi
 fi
 
 exec "$@"
